a1556e2ad2483d09ecbad076ed8345a7e1ed5905
Joe Maples <joe@maples.dev>
2024-07-21 09:42:16 -0400

    Revert "fix: Move with speed instead of lift_speed"
    This reverts commit 78e53f80177a01ff9fa66f1307754ac45e88d8ab.
    
    latest klipper updates broke this

--- auto_z_offset.py
+++ auto_z_offset.py
@@ -65,7 +65,7 @@ class AutoZOffsetCommandHelper(probe.ProbeCommandHelper):
         curpos[1] = 120
         if curpos[2] < 1.0:
             curpos[2] = self.probe_hop
-        self._move(curpos, params["speed"])
+        self._move(curpos, params["lift_speed"])
 
     def lift_probe(self, gcmd):
         toolhead = self.printer.lookup_object("toolhead")
@@ -115,7 +115,7 @@ class AutoZOffsetCommandHelper(probe.ProbeCommandHelper):
         curpos = toolhead.get_position()
         curpos[0] = 120 - main_probe.probe_offsets.x_offset
         curpos[1] = 120 - main_probe.probe_offsets.y_offset
-        self._move(curpos, params["speed"])
+        self._move(curpos, params["lift_speed"])
 
         # Use main probe to measure its own offset
         pos = probe.run_single_probe(main_probe, gcmd)
